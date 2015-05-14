da937519f0845c869c335f36a98f48c2602c4425
merlinoa <merlinoa88@gmail.com>
2015-05-14 18:15:44 -0400

    generic function for running emods

---
+++ R/emod.R
@@ -0,0 +1,27 @@
+#' emod
+#' 
+#' generic function for calculating emods under various assumtions
+#' 
+#' @param loss S3 object of one of the loss classes defined in the \code{emod} package
+#' @param payroll a list of n data frames.  Each list element contains a data frame with
+#' two columns.  The first column is the payroll class and the second column is the
+#' payroll dollar value.  Each data frame is in a seperate list item and contains a
+#' years worth of payroll.
+#' @param ... additional arguments
+#' 
+#' @export
+emod <- function(loss, payroll, ...) UseMethod("emod")
+
+
+
+#' NCCI emod
+#' 
+#' 
+emod.loss_ncci <- function(loss, payroll, emod_year = 2015) {
+  stopifnot(length(payroll) == 3)
+  
+  year_class_factors <- filter(class_factors, year == emod_year) %>%
+    select(-year)
+  
+  dplyr::left_join(payroll[1], year_class_factors)
+}
